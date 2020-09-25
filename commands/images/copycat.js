ce4985ac902fd7e20ccdd2fd0c2f81e6bbf7f12f
IllusionMan1212 <hisham.abourgheba@gmail.com>
2020-09-25 20:59:43 +0300

    fix fluff and add copy's catapi

---
+++ commands/images/copycat.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const request = require("request");
+
+exports.run = (client, message) => {
+    const baseUrl = "https://thatcopy.pw/catapi/get/webp";
+
+    request(baseUrl, function (error, _response, body) {
+        if (error) {
+            message.channel.send("Sorry something seems to have gone wrong!");
+            console.log(error);
+            return;
+        }
+
+        body = JSON.parse(body);
+        const imgURL = body.url;
+
+        const emb = new Discord.MessageEmbed();
+            emb.setColor(client.config.embedColor);
+            emb.setImage(imgURL);
+            console.log(imgURL);
+
+        message.channel.send(emb);
+    })
+}
+
+exports.help = {
+    enabled: true,
+    hideHelp: false,
+    type: "image",
+    name: "copycat",
+    description: "Gets a copy cat from copy's catAPI",
+    usage: "`yabe copycat`",
+}
